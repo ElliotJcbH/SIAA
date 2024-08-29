faafa09933656eaa217a70651bf8042b3199c7f0
Elliot Halili <elliot.jacob09@gmail.com>
2024-08-29 14:42:38 +0800

    Adding Base Filed

---
+++ scripts/sendMail.js
@@ -0,0 +1,9 @@
+function SendMail() {
+    let params = {
+        from_name: "Company Name",
+        username: document.getElementById(),
+    }
+
+    emailjs.send(service_cwpkviv, template_rdzi99l, params).then(alert);
+    // is .then() just a short version of if(0)? 
+}
